932ede76ad7f4008903837c6c04ed81d6390a8cb
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-03-15 00:39:13 +0200

    Add tasks

---
+++ JavaScript/Tasks/4-iterate.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Task: use `on` and `AsyncIterator` to iterate all items
+// in purchase emitted `into EventEmitter`
+
+const { EventEmitter, on } = require('node:events');
+
+const purchase = new EventEmitter({ captureRejections: true });
+
+const electronics = [
+  { name: 'Laptop', price: 1500 },
+  { name: 'Keyboard', price: 100 },
+  { name: 'HDMI cable', price: 10 },
+];
+
+const iterator = on(purchase, 'add');
+console.log({ iterator });
+
+for (const item of electronics) {
+  purchase.emit('add', item);
+}
+
