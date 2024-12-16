3796cee94a3660189440f392477f4c74cc489736
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-12-16 07:06:01 +0200

    Fix code style

--- JavaScript/8-once.js
+++ JavaScript/8-once.js
@@ -4,9 +4,11 @@ const { EventEmitter, once } = require('node:events');
 
 const emitter = new EventEmitter();
 
-(async () => {
+const main = async () => {
   const res = await once(emitter, 'name');
   console.log(res);
-})();
+};
+
+main();
 
 emitter.emit('name', { a: 4 }, { a: 5 }, { a: 6 });
